c5df21491e13a28303a21cc808053ff362d865f4
fahmi-ramadhan <fahmi122004@gmail.com>
2025-11-26 11:29:22 +0700

    feat: implements Author and Publisher resource

---
+++ app/Nova/Author.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\File;
+use Laravel\Nova\Fields\Avatar;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Trix;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Author extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\Author>
+     */
+    public static $model = \App\Models\Author::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+        'name',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @return array<int, \Laravel\Nova\Fields\Field>
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [
+            ID::make()
+                ->sortable(),
+            Avatar::make('Avatar')
+                ->showWhenPeeking()
+                ->rounded()
+                ->rules('required', File::image()->max(1024 * 10))
+                ->path('authors'),
+            Text::make('Name')
+                ->showWhenPeeking()
+                ->sortable()
+                ->rules('required', 'string', 'max:255'),
+            Trix::make('Biography')
+                ->showWhenPeeking()
+                ->fullWidth()
+                ->rules('required', 'string', 'max:10000'),
+
+        ];
+    }
+
+    /**
+     * Get the cards available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Card>
+     */
+    public function cards(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Filters\Filter>
+     */
+    public function filters(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Lenses\Lens>
+     */
+    public function lenses(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Actions\Action>
+     */
+    public function actions(NovaRequest $request): array
+    {
+        return [];
+    }
+}
