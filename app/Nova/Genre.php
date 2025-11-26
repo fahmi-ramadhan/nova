bc30e11daa643e8d98aa673efb1bbc5677df7aa1
fahmi-ramadhan <fahmi122004@gmail.com>
2025-11-26 16:01:20 +0700

    feat: add genre resource and update genre model

---
+++ app/Nova/Genre.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Genre extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\Genre>
+     */
+    public static $model = \App\Models\Genre::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+        'name',
+        'parent.name',
+    ];
+
+    public static $with = ['parent'];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @return array<int, \Laravel\Nova\Fields\Field>
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [
+            ID::make()
+                ->sortable(),
+            Text::make('Name')
+                ->sortable()
+                ->rules('required', 'string', 'max:60'),
+            BelongsTo::make('Parent', resource: Genre::class)
+                ->help('If this genre is a sub-genre, select the parent genre.')
+                ->nullable(),
+            HasMany::make('Books'),
+            HasMany::make('Sub-Genres', 'subGenres', Genre::class),
+        ];
+    }
+
+    public function fieldsForIndex(): array
+    {
+        return [
+            ID::make()
+                ->sortable(),
+
+            Text::make('Name', function () {
+                if ($this->parent) {
+                    return "{$this->name}/{$this->parent->name}";
+                }
+
+                return $this->name;
+            }),
+        ];
+    }
+
+    public function subtitle()
+    {
+        return $this->parent?->name ?? '-';
+    }
+
+    /**
+     * Get the cards available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Card>
+     */
+    public function cards(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Filters\Filter>
+     */
+    public function filters(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Lenses\Lens>
+     */
+    public function lenses(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Actions\Action>
+     */
+    public function actions(NovaRequest $request): array
+    {
+        return [];
+    }
+}
