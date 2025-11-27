79e60bd18a5d645a7cbc1e3c61b4c2f622ec48fd
fahmi-ramadhan <fahmi122004@gmail.com>
2025-11-27 11:18:21 +0700

    feat: implement review resource

---
+++ app/Nova/Review.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\MorphTo;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Review extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\Review>
+     */
+    public static $model = \App\Models\Review::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'title';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+        'title',
+        'stars',
+        'body',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @return array<int, \Laravel\Nova\Fields\Field>
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [
+            ID::make()
+                ->sortable(),
+            MorphTo::make('Reviewer')
+                ->types([
+                    User::class,
+                    Customer::class,
+                ]),
+            MorphTo::make('Reviewable')
+                ->types([
+                    Author::class,
+                    Book::class,
+                ]),
+            Text::make('Title')
+                ->rules('required', 'string', 'max:255'),
+            Text::make('Body')
+                ->rules('required', 'string', 'max:65535'),
+            Number::make('Stars')
+                ->rules('required', 'integer', 'min:1', 'max:5'),
+            DateTime::make('Verified At')
+                ->nullable()
+                ->hideFromIndex()
+                ->rules('nullable', 'date'),
+            Boolean::make('Verified', fn() => $this->verified_at !== null)
+                ->filterable()
+                ->exceptOnForms(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Card>
+     */
+    public function cards(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Filters\Filter>
+     */
+    public function filters(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Lenses\Lens>
+     */
+    public function lenses(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @return array<int, \Laravel\Nova\Actions\Action>
+     */
+    public function actions(NovaRequest $request): array
+    {
+        return [];
+    }
+}
